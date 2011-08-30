42b446b92234a253c72672cc93c1bfa3fed717e9
daniel.e.rossy@gmail.com <daniel.e.rossy@gmail.com@f578abcc-b9c8-adf4-a0c0-25998a80670a>
2011-08-30 20:21:04 +0000

    cambios a principal.py y eliminacion de imagenes gif, reemplazo por pngs.

--- trunk/principal.py
+++ trunk/principal.py
@@ -1,37 +1,67 @@
 import sys, pygame
 
 tamaño_cuadro=32
+velocidad = 16
 
 def cargarImagen(imagen):
     return pygame.image.load('./grafs/'+imagen).convert_alpha()
+
+class Globales():
+    VELOCIDAD=1
 
 class Hero(pygame.sprite.DirtySprite):
     ARRIBA =1
     DERECHA = 2
     ABAJO =3
     IZQUIERDA=4
+    variacion_velocidad=16
     def __init__(self):
         pygame.sprite.DirtySprite.__init__(self)
-        self.images=(cargarImagen('heroeF.png'),cargarImagen('heroeE.png'),cargarImagen('heroeI.png'),cargarImagen('heroeD.png'))
-        self.rects=[]
+        self.images=(cargarImagen('heroeE.png'),cargarImagen('heroeF.png'),cargarImagen('heroeI.png'),cargarImagen('heroeD.png'))
         self.dirty=1
-        self.image=self.images[0]
+        self.image=self.images[1]
         self.rect=self.image.get_rect()
         self.rect.y=8*tamaño_cuadro
         self.rect.x=8*tamaño_cuadro
     def mover(self,direccion):
+        delta=Globales.VELOCIDAD*self.variacion_velocidad
         if direccion==self.ARRIBA:
-            self.rect.y -= tamaño_cuadro
-            self.image=self.images[1]
-        if direccion==self.DERECHA:
-            self.rect.x += tamaño_cuadro
-            self.image=self.images[3]
-        if direccion==self.ABAJO:
-            self.rect.y += tamaño_cuadro
+            self.rect.y -= delta
             self.image=self.images[0]
+        if direccion==self.ABAJO:
+            self.rect.y += delta
+            self.image=self.images[1]
         if direccion==self.IZQUIERDA:
-            self.rect.x -= tamaño_cuadro
+            self.rect.x -= delta
             self.image=self.images[2]
+        if direccion==self.DERECHA:
+            self.rect.x += delta
+            self.image=self.images[3]
+        self.dirty=1
+
+class Enemy (pygame.sprite.DirtySprite):
+    ARRIBA =1
+    DERECHA = 2
+    ABAJO =3
+    IZQUIERDA=4
+    variacion_velocidad=16
+    def __init__(self):
+        pygame.sprite.DirtySprite.__init__(self)
+        self.image=cargarImagen('Enemy.png')
+        self.rect=self.image.get_rect()
+        self.dirty=1
+        self.rect.y=16*tamaño_cuadro
+        self.rect.x=16*tamaño_cuadro
+    def mover(self,direccion):
+        delta=Globales.VELOCIDAD*self.variacion_velocidad
+        if direccion==self.ARRIBA:
+            self.rect.y -= delta
+        if direccion==self.ABAJO:
+            self.rect.y += delta
+        if direccion==self.IZQUIERDA:
+            self.rect.x -= delta
+        if direccion==self.DERECHA:
+            self.rect.x += delta
         self.dirty=1
 
 pygame.init()
@@ -47,18 +77,13 @@ for i in range(len(bg)):
 back=pantalla.convert()
 
 heroe=Hero()
-gHeroe=pygame.sprite.LayeredDirty(heroe)
-gHeroe.clear(pantalla,back)
-pygame.display.update(gHeroe.draw(pantalla))
+enemigo=Enemy()
 
+gSprites=pygame.sprite.RenderUpdates(heroe,enemigo)
+gSprites.draw(pantalla)
 
-#pygame.key.set_repeat(100000,100000)
-FPSc=pygame.time.Clock()
-FPS=pygame.font.Font(pygame.font.match_font('Courier New,Arial'),12)
-#print(pygame.K_UP)
 running=1
-while running==1:
-    FPSc.tick(60)
+while running==1:    
     for event in pygame.event.get():
         if event.type == pygame.QUIT: running=0
         if event.type==pygame.KEYDOWN:
@@ -66,16 +91,7 @@ while running==1:
             if event.key==pygame.K_DOWN:heroe.mover(Hero.ABAJO)
             if event.key==pygame.K_LEFT:heroe.mover(Hero.IZQUIERDA)
             if event.key==pygame.K_RIGHT:heroe.mover(Hero.DERECHA)
-##    pygame.event.pump()
-##    tecla=pygame.key.get_pressed()
-##    if tecla[pygame.K_UP]:heroe.mover(Hero.ARRIBA)
-##    if tecla[pygame.K_DOWN]:heroe.mover(Hero.ABAJO)
-##    if tecla[pygame.K_LEFT]:heroe.mover(Hero.IZQUIERDA)
-##    if tecla[pygame.K_RIGHT]:heroe.mover(Hero.DERECHA)
     
-    updateList = gHeroe.draw(pantalla)
+    pygame.display.update()
 
-    updateList.append(pantalla.blit(FPS.render(str(int(FPSc.get_fps())),False,(0,0,0),(255,255,255)),(150,0)))
-    pygame.display.update(updateList)
-#    print( FPSc.get_fps())
 pygame.quit()
