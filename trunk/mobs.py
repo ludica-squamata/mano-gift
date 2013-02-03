1197f54b918914834a75b9a0802d4596ee29a3f7
moguchicorp@gmail.com <moguchicorp@gmail.com@f578abcc-b9c8-adf4-a0c0-25998a80670a>
2013-02-03 23:47:08 +0000

    un monton de cambios para movimiento con scrolling
    el mapa se frena cuando llega al borde y entonces se mueve el hero
    -falta control de colisiones

--- trunk/mobs.py
+++ trunk/mobs.py
@@ -1,35 +1,15 @@
 from pygame import sprite
 from misc import Resources as r
+from globs import Constants as C
+from base import _giftSprite
 
-class MobGroup (sprite.LayeredDirty):
-    '''Grupo Básico para todos los Mobs'''
-    
-    def __init__ (self,*sprites):
-        super().__init__(*sprites)
-
-class Mob (sprite.DirtySprite):
+class Mob (_giftSprite):
     '''Clase base para todos los Mobs'''
-    def __init__(self,ruta):
-        super().__init__()
-        self.x = 0
-        self.y = 0
-        self.pos = self.x,self.y
-        self.velocidad = 32
-        self.sprite = r.cargar_imagen(ruta)
-        self.rect = self.sprite.get_rect()
-        
-    def reubicar (self,x,y):
-        self.x += x*self.velocidad
-        self.y += y*self.velocidad
-        self.pos = self.x,self.y
-        
+    velocidad = 0
 
 class PC (Mob):
-    def __init__(self,ruta):
-        super().__init__(ruta)
-        self.x = 256
-        self.y = 256
-        self.pos = self.x,self.y
+    centroX = 0
+    centroY = 0
 
 class Enemy (Mob):
     def __init__(self):
@@ -39,11 +19,10 @@ class NPC (Mob):
     def __init__(self):
         super().__init__()
 
-class Inventory:
+class Inventory(object):
     # la mochila
     pass
 
-class Items:
+class Items(object):
     #para cosas que van en el inventario
     pass
-
