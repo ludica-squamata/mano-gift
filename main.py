978657a9a866ffd7e3ee67e4938e5dca7cf5bb7d
daniel.e.rossy@gmail.com <daniel.e.rossy@gmail.com@f578abcc-b9c8-adf4-a0c0-25998a80670a>
2013-04-29 01:47:07 +0000

    Deshechos los cambios anteriores, aunque manteniendo algo de funcionalidad: - el NPC no se mueve cuando le hablan, así como tampoco lo hace el héroe. - disolución del módulo mododejuego.py, ahora integrado a main.py - se mantienen los bugs de la versión anterior.

--- main.py
+++ main.py
@@ -3,89 +3,87 @@ from mapa import Stage,Prop
 from mobs import PC
 from misc import Resources as r
 from globs import Constants as C, World as W,FPS
-from mododejuego import ModoDeJuego
 
 configs = r.abrir_json('config.json')
 
 pygame.init()
-Juego = ModoDeJuego('aventura')
 tamanio = C.ALTO, C.ANCHO
 pantalla = pygame.display # screen
 fondo = pantalla.set_mode(tamanio) # surface
-#FPS = pygame.time.Clock()
 pygame.key.set_repeat(30,15)
 
 W.cargar_hero()
 W.setear_mapa(configs['mapa_inicial'], 'inicial')
 
-i = 10
-while i > 0:
-    FPS.tick(60)
-    i-=1
+inAction = False
+onDialog = False
 
 while True:
     FPS.tick(60)
-    Juego.mainloop()
-    #for event in pygame.event.get():
-    #    if event.type == pygame.QUIT:
-    #        sys.exit()
-    #    
-    #    elif event.type == pygame.KEYUP:
-    #        if event.key == C.TECLAS.ACCION:
-    #            inAction = False
-    #            
-    #
-    #    elif event.type == pygame.KEYDOWN:
-    #        dx,dy = 0,0
-    #        if event.key == C.TECLAS.IZQUIERDA: 
-    #            W.HERO.cambiar_direccion('derecha')
-    #            dx +=1
-    #
-    #        elif event.key == C.TECLAS.DERECHA:
-    #            W.HERO.cambiar_direccion('izquierda')
-    #            dx -= 1
-    #
-    #        elif event.key == C.TECLAS.ARRIBA:
-    #            W.HERO.cambiar_direccion('arriba')
-    #            dy += 1
-    #
-    #        elif event.key == C.TECLAS.ABAJO:
-    #            W.HERO.cambiar_direccion('abajo')
-    #            dy -= 1
-    #        
-    #        elif event.key == C.TECLAS.ACCION:
-    #            if not inAction:
-    #                W.HERO.accion()
-    #                inAction = True
-    #        
-    #        elif event.key == C.TECLAS.INVENTARIO:
-    #            if not inAction:
-    #                W.HERO.ver_inventario()
-    #                inAction = True
-    #                
-    #        elif event.key == C.TECLAS.MENU: # debug
-    #            os.system(['clear','cls'][os.name == 'nt'])
-    #            print('Este sería el menú :P')
-    #            #print(W.mapas)
-    #            #print(W.MAPA_ACTUAL)
-    #            #print(W.MAPA_ACTUAL.data)
-    #            #for x in W.MAPA_ACTUAL.contents.get_sprites_from_layer(C.CAPA_GROUND_ITEMS):
-    #            #    print(x.mask.overlap(W.HERO.mask,(x.mapX - W.HERO.mapX, x.mapY - (W.HERO.mapY - dy))))
-    #            #    print(W.HERO.mask.overlap(x.mask,(x.mapX - W.HERO.mapX, x.mapY - (W.HERO.mapY - dy))))
-    #            #    print(x.rect, x.mask.get_size())
-    #            #
-    #            print (W.HERO.rect)
-    #            print ("map_x:",W.HERO.mapX, "map_y:",W.HERO.mapY,'\n')
-    #            print ("Gx:",str(int(W.HERO.mapX/32)), "Gy:",str(int(W.HERO.mapY/32)),'\n')
-    #            print(W.HERO.direccion)
-    #            
-    #        
-    #        elif event.key == C.TECLAS.SALIR:
-    #            pygame.quit()
-    #            print('Saliendo...')
-    #            sys.exit()
-    #
-    #        W.MAPA_ACTUAL.mover(dx,dy)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        
+        elif event.type == pygame.KEYUP:
+            if event.key == C.TECLAS.ACCION:
+                inAction = False
+                
+        elif event.type == pygame.KEYDOWN:
+            dx,dy = 0,0
+            if not onDialog:
+                if event.key == C.TECLAS.IZQUIERDA: 
+                    W.HERO.cambiar_direccion('derecha')
+                    dx +=1
+        
+                elif event.key == C.TECLAS.DERECHA:
+                    W.HERO.cambiar_direccion('izquierda')
+                    dx -= 1
+        
+                elif event.key == C.TECLAS.ARRIBA:
+                    W.HERO.cambiar_direccion('arriba')
+                    dy += 1
+        
+                elif event.key == C.TECLAS.ABAJO:
+                    W.HERO.cambiar_direccion('abajo')
+                    dy -= 1
+            
+            if event.key == C.TECLAS.ACCION:
+                if not inAction:
+                    onDialog = W.HERO.accion()
+                    inAction = True
+            
+            elif event.key == C.TECLAS.INVENTARIO:
+                if not inAction:
+                    W.HERO.ver_inventario()
+                    inAction = True
+                    
+            elif event.key == C.TECLAS.MENU: # debug
+                os.system(['clear','cls'][os.name == 'nt'])
+                print('Este sería el menú :P')
+                #print(W.mapas)
+                #print(W.MAPA_ACTUAL)
+                #print(W.MAPA_ACTUAL.data)
+                #for x in W.MAPA_ACTUAL.contents.get_sprites_from_layer(C.CAPA_GROUND_ITEMS):
+                #    print(x.mask.overlap(W.HERO.mask,(x.mapX - W.HERO.mapX, x.mapY - (W.HERO.mapY - dy))))
+                #    print(W.HERO.mask.overlap(x.mask,(x.mapX - W.HERO.mapX, x.mapY - (W.HERO.mapY - dy))))
+                #    print(x.rect, x.mask.get_size())
+                #
+                print (W.HERO.rect)
+                print ("map_x:",W.HERO.mapX, "map_y:",W.HERO.mapY,'\n')
+                print ("Gx:",str(int(W.HERO.mapX/32)), "Gy:",str(int(W.HERO.mapY/32)),'\n')
+                print(W.HERO.direccion)
+            
+            elif event.key == C.TECLAS.SALIR:
+                    pygame.quit()
+                    print('Saliendo...')
+                    sys.exit()
+            
+            elif event.key == C.TECLAS.CONTROL:
+                if onDialog:
+                    W.MAPA_ACTUAL.dialogs.empty()
+                    onDialog = False
+
+            W.MAPA_ACTUAL.mover(dx,dy)
 
     cambios = W.MAPA_ACTUAL.render(fondo)
     pantalla.update(cambios)
