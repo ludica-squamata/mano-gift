8cf039082aad7abdc6eab103562e7575667c85d4
daniel.e.rossy@gmail.com <daniel.e.rossy@gmail.com@f578abcc-b9c8-adf4-a0c0-25998a80670a>
2013-06-12 16:50:22 +0000

    Posibilidad de pausar el juego entrando al menú con la tecla Enter Agregado un marco estético a los diálogos y ventana de menú Ahora es posible cerrar el diálogo de inventario con la misma tecla.

--- trunk/UI.py
+++ trunk/UI.py
@@ -3,34 +3,69 @@ from libs.textrect import render_textrect
 from globs import Constants as C, World as W
 from base import _giftSprite
 
-class Dialog (_giftSprite):
-    fg_color = 0,125,255
+class Ventana (_giftSprite):
+    canvas = None
+    bg_cnvs = 125,125,125 # gris
     bg_color = 0,0,0
-    fuente = None
-    sel = 1
-    def __init__(self, texto):
-        self.fuente = pygame.font.SysFont('verdana', 16)
-        rect = pygame.Rect((30, 325, C.ANCHO, int(C.ALTO/5)))
-        image = render_textrect(texto,self.fuente,rect,self.fg_color,self.bg_color)
-        super().__init__(image,x=rect.x, y=rect.y)
-        self.rect = pygame.Rect((0, 384, C.ANCHO, int(C.ALTO/5)))
-        self.dirty = 2
-
-    def setText(self, texto):
-        rect = pygame.Rect((30, 325, C.ANCHO, int(C.ALTO/5)))
-        if W.onSelect:
-            render = render_textrect(texto,self.fuente,rect,self.fg_color,self.bg_color)
-            pygame.draw.line(render,self.fg_color,(0,self.sel*20),(470,self.sel*20))
-            self.image = render
-        else:
-            self.image = render_textrect(texto,self.fuente,rect,self.fg_color,self.bg_color)
+    fg_color = 0,125,255
+    posicion = x,y = 0,0
+    tamanio = ancho,alto = 0,0
+    def __init__(self,image):
+        super().__init__(image)
     
     def elegir_opcion(self,j,m):
         self.sel += j
         if self.sel < 1: self.sel = 1
         elif self.sel > 2: self.sel = 2
-        pygame.draw.line(self.image,self.fg_color,(0,self.sel*20),(470,self.sel*20))
-        pygame.draw.line(self.image,self.bg_color,(0,(self.sel+m)*20),(470,(self.sel+m)*20))
+        pygame.draw.line(self.image,self.fg_color,(3,self.sel*22),(C.ANCHO-5,self.sel*22))
+        pygame.draw.line(self.image,self.bg_color,(3,(self.sel+m)*22),(C.ANCHO-5,(self.sel+m)*22))
 
         return self.sel
+    
+class Dialog (Ventana):
+    fuente = None
+    sel = 1
+    posicion = x,y = 0,384
+    def __init__(self, texto):
+        self.canvas = pygame.Surface((int(C.ANCHO), int(C.ALTO/5)))
+        self.canvas.fill(self.bg_cnvs)
+        
+        self.fuente = pygame.font.SysFont('verdana', 16)
+        rect = pygame.Rect((self.posicion, (C.ANCHO-7, int(C.ALTO/5)-7)))
+        image = render_textrect(texto,self.fuente,rect,self.fg_color,self.bg_color)
+        
+        self.canvas.blit(image,(3,3))
+        super().__init__(self.canvas)
+        self.ubicar(*rect.topleft)
+        self.dirty = 2
 
+    def setText(self, texto):
+        rect = pygame.Rect((3, 3, C.ANCHO-7, int(C.ALTO/5)-7))
+        if W.onSelect:
+            render = render_textrect(texto,self.fuente,rect,self.fg_color,self.bg_color)
+            self.canvas.blit(render,rect)
+            pygame.draw.line(self.canvas,self.fg_color,(0,self.sel*22),(C.ANCHO-7,self.sel*22))
+            self.image = self.canvas
+        else:
+            render = render_textrect(texto,self.fuente,rect,self.fg_color,self.bg_color)
+            self.canvas.blit(render,rect)
+            self.image = self.canvas
+
+class Menu (Ventana):
+    def __init__(self):
+        self.canvas = pygame.Surface((int(C.ANCHO)-20, int(C.ALTO)-20))
+        clip = pygame.Rect(3,3,int(C.ANCHO)-27, int(C.ALTO)-27)
+        self.canvas.fill(self.bg_cnvs)
+        self.canvas.fill(self.bg_color,rect=clip)
+        
+        fuente = pygame.font.SysFont('verdana', 16)
+        fuente.set_underline(True)
+        rect = pygame.Rect((-1,-1),(int(C.ANCHO)-27,30))
+        titulo =  render_textrect('Pausa',fuente,rect,self.fg_color,self.bg_color,1)
+        self.canvas.blit(titulo,(3,3))
+        
+        super().__init__(self.canvas)
+        self.ubicar(10,10)
+        self.dirty = 2
+        W.onPause = True
+    
--- trunk/main.py
+++ trunk/main.py
@@ -10,14 +10,12 @@ pygame.init()
 tamanio = C.ALTO, C.ANCHO
 pantalla = pygame.display # screen
 fondo = pantalla.set_mode(tamanio) # surface
-pygame.key.set_repeat(30,15)
+pygame.key.set_repeat(50,15)
 
 W.cargar_hero()
 W.setear_mapa(configs['mapa_inicial'], 'inicial')
 
 inAction = False
-#FPS_f = pygame.font.Font(pygame.font.match_font('Courier New,Arial'),20)
-onSelect = False
 while True:
     FPS.tick(60)
     for event in pygame.event.get():
@@ -30,32 +28,37 @@ while True:
 
         elif event.type == pygame.KEYDOWN:
             dx,dy = 0,0
-            if W.onDialog:
-                if W.onSelect:
-                    if event.key == C.TECLAS.ARRIBA:
+            if event.key == C.TECLAS.IZQUIERDA:
+                if not W.onPause:
+                    if not W.onDialog:
+                        W.HERO.cambiar_direccion('derecha')
+                        dx +=1
+
+            elif event.key == C.TECLAS.DERECHA:
+                if not W.onPause:
+                    if not W.onDialog:
+                        W.HERO.cambiar_direccion('izquierda')
+                        dx -= 1
+
+            elif event.key == C.TECLAS.ARRIBA:
+                if W.onDialog:
+                    if W.onSelect:
                         W.HERO.cambiar_opcion_dialogo(-1)
-        
-                    elif event.key == C.TECLAS.ABAJO:
+                else:
+                    if not W.onPause:
+                        W.HERO.cambiar_direccion('arriba')
+                        dy += 1
+
+            elif event.key == C.TECLAS.ABAJO:
+                if W.onDialog:
+                    if W.onSelect:
                         W.HERO.cambiar_opcion_dialogo(+1)
-                                        
-            else:
-                if event.key == C.TECLAS.IZQUIERDA:
-                    W.HERO.cambiar_direccion('derecha')
-                    dx +=1
-    
-                elif event.key == C.TECLAS.DERECHA:
-                    W.HERO.cambiar_direccion('izquierda')
-                    dx -= 1
-    
-                elif event.key == C.TECLAS.ARRIBA:
-                    W.HERO.cambiar_direccion('arriba')
-                    dy += 1
-    
-                elif event.key == C.TECLAS.ABAJO:
-                    W.HERO.cambiar_direccion('abajo')
-                    dy -= 1
+                else:
+                    if not W.onPause:
+                        W.HERO.cambiar_direccion('abajo')
+                        dy -= 1
                     
-            if event.key == C.TECLAS.ACCION:
+            elif event.key == C.TECLAS.ACCION:
                 if not inAction:
                     W.HERO.accion()
                     inAction = True
@@ -78,24 +81,23 @@ while True:
                     W.HERO.ver_inventario()
                     W.onDialog = True
                     inAction = True
-                        
-
-            elif event.key == C.TECLAS.MENU: # debug
-                os.system(['clear','cls'][os.name == 'nt'])
-                print('Este sería el menú :P')
-                #print(W.mapas)
-                #print(W.MAPA_ACTUAL)
-                #print(W.MAPA_ACTUAL.data)
-                #for x in W.MAPA_ACTUAL.contents.get_sprites_from_layer(C.CAPA_GROUND_ITEMS):
-                #    print(x.mask.overlap(W.HERO.mask,(x.mapX - W.HERO.mapX, x.mapY - (W.HERO.mapY - dy))))
-                #    print(W.HERO.mask.overlap(x.mask,(x.mapX - W.HERO.mapX, x.mapY - (W.HERO.mapY - dy))))
-                #    print(x.rect, x.mask.get_size())
-                #
-                print (W.HERO.rect)
-                print ("map_x:",W.HERO.mapX, "map_y:",W.HERO.mapY,'\n')
-                print ("Gx:",str(int(W.HERO.mapX/32)), "Gy:",str(int(W.HERO.mapY/32)),'\n')
-                print(W.HERO.direccion)
-
+                else:
+                    if not W.onPause:
+                        inAction = False
+                        W.onDialog = False
+                        W.MAPA_ACTUAL.endDialog()
+                    
+            elif event.key == C.TECLAS.MENU:
+                if not inAction:
+                    if not W.onPause:
+                        W.MAPA_ACTUAL.popMenu()
+                        inAction = True
+                else:
+                    inAction = False
+                    W.onPause = False
+                    W.onDialog = False
+                    W.MAPA_ACTUAL.endDialog()
+                
             elif event.key == C.TECLAS.SALIR:
                     pygame.quit()
                     print('Saliendo...')
@@ -104,5 +106,4 @@ while True:
             W.MAPA_ACTUAL.mover(dx,dy)
 
     cambios = W.MAPA_ACTUAL.render(fondo)
-    #fondo.blit((FPS_f.render(str(int(FPS.get_fps())),False,(255,255,255,255))),(0,0))
     pantalla.update(cambios)
