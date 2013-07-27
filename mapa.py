651333cbdcc4e110a9c9bf9f24f37a42c5cca78e
moguchicorp@gmail.com <moguchicorp@gmail.com@f578abcc-b9c8-adf4-a0c0-25998a80670a>
2013-07-27 18:17:51 +0000

    quitados eval() en prop que no tienen nada que hacer ahi

--- mapa.py
+++ mapa.py
@@ -13,34 +13,38 @@ class Prop (_giftSprite):
         Estos objetos no se pueden mover por si mismos, pero usualmente tienen
         algún tipo de interacción (agarrar, mover, cortar, etc).'''
         
-    propiedades = [] #indica si es agarrable, cortable, movible, etc.
+    _propiedades = None #indica si es agarrable, cortable, movible, etc.
+    _operable = None
     estado = 0
     def __init__ (self,nombre, imagen, stage, x,y, data = None):
         super().__init__(imagen,stage,x*C.CUADRO,y*C.CUADRO)
         self.nombre = nombre
         self.estado = 0
+        self._propiedades = {}
         if data != None:
             for prop in data['propiedades']:
-                if prop == 'operable':
-                    for var in data['propiedades'][prop][str(self.estado)]:
-                        exec('self.'+var+'=True')
-                exec('self.'+prop+'=True')
+                self._propiedades[prop] = True;
+            if self.propiedades('operable'):
+                self._operable = [(data['propiedades'][prop]['0']['solido'],data['propiedades'][prop]['0']['visible']), (data['propiedades'][prop]['1']['solido'],data['propiedades'][prop]['1']['visible'])]
+                self.solido = self._operable[0][0]
+                self.visible = self._operable[0][1]
 
     def interaccion(self,x=0,y=0):
-        if self._eval_prop('agarrable'):
+        if self.propiedades('agarrable'):
             return self.nombre
         
-        elif self._eval_prop('operable'):
+        elif self.propiedades('operable'):
             if self.estado == 0:
                 self.estado = 1
             else:
                 self.estado = 0
+            print((self.solido,self.visible))
+            self.solido = self._operable[self.estado][0]
+            self.visible = self._operable[self.estado][1]
+            print((self.solido,self.visible))
 
-            self.visible = not self.visible
-            self.solido = not self.solido   
-
-        elif self._eval_prop('empujable'):
-            if self._eval_prop('pesado'):
+        elif self.propiedades('empujable'):
+            if self.propiedades('pesado'):
                 if x > 0: x -= 10
                 elif x < 0: x += 10
     
@@ -49,14 +53,9 @@ class Prop (_giftSprite):
             
             self.reubicar(x,y)
     
-    def _eval_prop(self,propiedad):
-        try:
-            exec('self.'+propiedad)
-            return True
-            #pero, que ocurre si la propiedad es False?
-            
-        except AttributeError:
-            return False
+    #devuelve true o false
+    def propiedades(self,propiedad):
+        return(propiedad in self._propiedades and self._propiedades[propiedad])
     
 class Stage:
     contents = None
