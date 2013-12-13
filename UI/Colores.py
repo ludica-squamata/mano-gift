babc2cbce572a298438f89ede36d81e67f90d975
daniel.e.rossy@gmail.com <daniel.e.rossy@gmail.com@f578abcc-b9c8-adf4-a0c0-25998a80670a>
2013-12-13 21:36:00 +0000

    Cambio estético: * Los ítems no consumibles (como los equipables, y próximamente los key ítems) aparecen en un tono de gris en el menú Ítems, pero en el blanco usual en Equipo.

--- UI/Colores.py
+++ UI/Colores.py
@@ -1,8 +1,8 @@
 from pygame import Color
 
 class Colores:
-    button_bg_color = Color(100,100,100)
     font_high_color = Color(255,255,255)
+    font_low_color = Color(200,200,200)
     font_none_color = Color(0,0,0)
     bg_cnvs = Color(125,125,125)
     bg_bisel_bg = Color(175,175,175)
--- UI/_item_inv.py
+++ UI/_item_inv.py
@@ -7,19 +7,19 @@ class _item_inv (Colores,_giftSprite):
     nombre = ''
     item = None
     
-    def __init__(self,item,ancho,pos,fuente):
+    def __init__(self,item,ancho,pos,fuente,color):
         self.item = item
         self.nombre = self.item.nombre.capitalize()
         cant = self.item.cantidad
-        imagen = self.construir_fila(ancho,self.nombre,cant,fuente)
+        imagen = self.construir_fila(ancho,self.nombre,cant,fuente,color)
         super().__init__(imagen)
         self.rect = self.image.get_rect(topleft=pos)
         self.dirty = 1
         
-    def construir_fila(self,ancho,nombre,cantidad,fuente):
-        _rect = Rect((-1,-1),(int(ancho/2),fuente.get_height()+1))
-        img_nmbr = render_textrect(nombre,fuente,_rect,self.font_high_color,self.bg_cnvs,1)
-        img_cant = render_textrect('x'+str(cantidad),fuente,_rect,self.font_high_color,self.bg_cnvs,1)
+    def construir_fila(self,ancho,nombre,cantidad,fuente,color):
+        _rect = Rect((-1,-1),(int(ancho/2),fuente.get_height()+1))        
+        img_nmbr = render_textrect(nombre,fuente,_rect,color,self.bg_cnvs,1)
+        img_cant = render_textrect('x'+str(cantidad),fuente,_rect,color,self.bg_cnvs,1)
         
         image = Surface((ancho,_rect.h))
         image.fill(self.bg_cnvs)
