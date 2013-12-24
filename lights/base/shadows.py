548c2ba16438985b1f4bbe9bafede1f564c876eb
moguchicorp@gmail.com <moguchicorp@gmail.com@f578abcc-b9c8-adf4-a0c0-25998a80670a>
2013-12-24 00:39:16 +0000

    comments

---
+++ lights/base/shadows.py
@@ -0,0 +1,17 @@
+import _giftSprite
+    
+class _shadowSprite(_giftSprite):
+    _sombras = None #list
+    proyectaSombra = True
+    
+    def recibir_luz(self, source):
+        if self.proyectaSombra:
+            #calcular direccion de origen
+            #marcar direccion como iluminada
+    def updateSombra(self):
+             #
+     
+            #generar sombra en direccion contraria a los slots iluminados
+            #si cambio la lista,
+            #actualizar imagen de sombra y centrar
+            pass
