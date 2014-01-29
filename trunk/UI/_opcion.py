cf4258d9cbe8d7378877f2ed64b26a8c029b7e19
daniel.e.rossy@gmail.com <daniel.e.rossy@gmail.com@f578abcc-b9c8-adf4-a0c0-25998a80670a>
2014-01-29 16:46:34 +0000

    docstrings..

--- trunk/UI/_opcion.py
+++ trunk/UI/_opcion.py
@@ -5,6 +5,10 @@ from pygame import font, Rect
 from libs.textrect import render_textrect
 
 class _opcion (Colores,_giftSprite):
+    '''Opción única que es parte de una lista más grande.
+    No tiene cantidad, pero puede ser seleccionada (resaltando)
+    mediante serElegido.'''
+    
     isSelected = False
     
     def __init__(self,texto,ancho,pos,size=16,aling=0):
@@ -20,29 +24,36 @@ class _opcion (Colores,_giftSprite):
         self.serDeselegido()
         
     def serElegido(self):
+        '''Cambia la imagen a la versión resaltada'''
         self.image = self.img_high
         self.isSelected = True
         self.rect = self.img_high.get_rect(topleft=self.pos)
         self.dirty = 1
         
     def serDeselegido(self):
+        '''Cambia la imagen a la versión no elegida'''
+        
         self.image = self.img_none
         self.isSelected = False
         self.rect = self.img_none.get_rect(topleft=self.pos)
         self.dirty = 1
     
     def setText(self,texto):
+        '''Cambia y asigna el texto de la opción'''
+        
         f = self.fuente
         r = self.rect
         bg = self.bg_cnvs
         a = self.aling
-        ok = False
-        while ok == False:
+        
+        while True:
             try:
+                #Si el texto es muy grande para el rect...
                 self.img_none = render_textrect(texto,f,r,self.font_none_color,bg,a)
                 self.img_high = render_textrect(texto,f,r,self.font_high_color,bg,a)
-                ok = True
+                break
             except TextRectException:
+                # Acá se achica.
                 self.size -= 1
                 f = font.SysFont('verdana', self.size)
                 
