14d70cc0ea19b7fadb6ff1879e1ac32b7de1fb10
daniel.e.rossy@gmail.com <daniel.e.rossy@gmail.com@f578abcc-b9c8-adf4-a0c0-25998a80670a>
2014-05-19 20:32:27 +0000

    Nueva clase _loader: - Movidos todos los cargar_# de stage a _loader. - Stage ahora llama a _loader.loadEverything para hacer todos    los cargar_#. - Esto es parte del issue 52.
    Update issue 52
    Movidos todos los cargar_# de stage a la nueva clase _loader.

--- mapa/Stage.py
+++ mapa/Stage.py
@@ -18,79 +18,10 @@ class Stage:
         self.mapa = ChunkMap(self.data) # por ahora es uno solo.
         self.grilla = generar_grilla(self.mapa.mask,self.mapa.image)
         self.properties = LayeredDirty()
-        self.cargar_hero(entrada)
-        self.cargar_props('ground')
-        self.cargar_props('top')
-        self.cargar_mobs(Enemy)
-        self.cargar_mobs(NPC)
-        self.cargar_quests()
-        self.cargar_salidas()
+        _loader.setStage(self)
+        _loader.loadEverything(entrada)
         self.addProperty(T.noche,C.CAPA_TOP_CIELO)
 
-    def cargar_props (self,capa):
-        imgs = self.data['refs']
-        POS = self.data['capa_'+capa]['props']
-        data = r.abrir_json('data/scripts/props.json')
-        if capa == 'ground':
-            layer = C.CAPA_GROUND_ITEMS
-        elif capa == 'top':
-            layer = C.CAPA_TOP_ITEMS
-        
-        for ref in POS:
-            for x,y in POS[ref]:
-                if ref in data:
-                    if 'image' in data[ref]:
-                        imagen = data[ref]['image']
-                    else:
-                        imagen = imgs[ref]
-                    
-                    prop = Prop(ref,imagen,self,x,y,data[ref])
-                else:
-                    prop = Prop(ref,imgs[ref],stage=self,x=x,y=y)
-                self.addProperty(prop,layer)
-                if prop.sombra != None:
-                    self.mapa.image.blit(prop.sombra,[x*C.CUADRO,y*C.CUADRO])
-
-    def cargar_mobs(self,clase):
-        if clase == Enemy:
-            pos = self.data['capa_ground']['mobs']['enemies']
-            act = 'agressive'
-        elif clase == NPC:
-            pos = self.data['capa_ground']['mobs']['npcs']
-            act = 'passive'
-        imgs = self.data['refs']
-
-        for ref in pos:
-            base = r.abrir_json('data/mobs/'+act+'.mob')
-            try:
-                data = r.abrir_json('data/mobs/'+ref+'.mob')
-            except IOError:
-                data = {}
-            base.update(data)
-            for x,y in pos[ref]:
-                mob = clase(ref,imgs[ref],self,x,y,base)
-                self.addProperty(mob,C.CAPA_GROUND_MOBS)
-                MobGroup.add(mob)
-
-    def cargar_hero(self, entrada = None):
-        W.HERO = PC('heroe',r.abrir_json('data/mobs/hero.mob'),self)
-        if entrada != None:
-            if entrada in self.data['entradas']:
-                x,y = self.data['entradas'][entrada]
-                W.HERO.ubicar(x*C.CUADRO, y*C.CUADRO)
-                self.addProperty(W.HERO,C.CAPA_HERO)
-    
-    def cargar_quests(self):
-        if 'quests' in self.data:
-            for quest in self.data['quests']:
-                QuestManager.add(quest)
-            
-    def cargar_salidas(self):
-        salidas = self.data['salidas']
-        for salida in salidas:
-            sld = Salida(salidas[salida])
-            self.properties.add(sld,layer=C.CAPA_GROUND_SALIDAS)
-    
     def addProperty(self,obj,_layer):
         self.properties.add(obj,layer =_layer)
     
@@ -117,6 +48,92 @@ class Stage:
         self.actualizar_grilla()
         self.mapa.dirty = 1
 
+class _loader:
+    STAGE = None
+    
+    @staticmethod
+    def setStage(stage):
+        _loader.STAGE = stage
+    
+    @staticmethod
+    def loadEverything(entrada):
+        _loader.cargar_hero(entrada)
+        _loader.cargar_props('ground')
+        _loader.cargar_props('top')
+        _loader.cargar_mobs(Enemy)
+        _loader.cargar_mobs(NPC)
+        _loader.cargar_quests()
+        _loader.cargar_salidas()
+        
+    @staticmethod
+    def cargar_props (capa):
+        imgs = _loader.STAGE.data['refs']
+        POS = _loader.STAGE.data['capa_'+capa]['props']
+        data = r.abrir_json('data/scripts/props.json')
+        if capa == 'ground':
+            layer = C.CAPA_GROUND_ITEMS
+        elif capa == 'top':
+            layer = C.CAPA_TOP_ITEMS
+        
+        for ref in POS:
+            for x,y in POS[ref]:
+                if ref in data:
+                    if 'image' in data[ref]:
+                        imagen = data[ref]['image']
+                    else:
+                        imagen = imgs[ref]
+                    
+                    prop = Prop(ref,imagen,_loader.STAGE,x,y,data[ref])
+                else:
+                    prop = Prop(ref,imgs[ref],stage=_loader.STAGE,x=x,y=y)
+                _loader.STAGE.addProperty(prop,layer)
+                if prop.sombra != None:
+                    _loader.STAGE.mapa.image.blit(prop.sombra,[x*C.CUADRO,y*C.CUADRO])
+    
+    @staticmethod
+    def cargar_mobs(clase):
+        if clase == Enemy:
+            pos = _loader.STAGE.data['capa_ground']['mobs']['enemies']
+            act = 'agressive'
+        elif clase == NPC:
+            pos = _loader.STAGE.data['capa_ground']['mobs']['npcs']
+            act = 'passive'
+        imgs = _loader.STAGE.data['refs']
+
+        for ref in pos:
+            base = r.abrir_json('data/mobs/'+act+'.mob')
+            try:
+                data = r.abrir_json('data/mobs/'+ref+'.mob')
+            except IOError:
+                data = {}
+            base.update(data)
+            for x,y in pos[ref]:
+                mob = clase(ref,imgs[ref],_loader.STAGE,x,y,base)
+                _loader.STAGE.addProperty(mob,C.CAPA_GROUND_MOBS)
+                MobGroup.add(mob)
+    
+    @staticmethod
+    def cargar_hero(entrada = None):
+        W.HERO = PC('heroe',r.abrir_json('data/mobs/hero.mob'),_loader.STAGE)
+        if entrada != None:
+            if entrada in _loader.STAGE.data['entradas']:
+                x,y = _loader.STAGE.data['entradas'][entrada]
+                W.HERO.ubicar(x*C.CUADRO, y*C.CUADRO)
+                _loader.STAGE.addProperty(W.HERO,C.CAPA_HERO)
+    
+    @staticmethod
+    def cargar_quests():
+        if 'quests' in _loader.STAGE.data:
+            for quest in _loader.STAGE.data['quests']:
+                QuestManager.add(quest)
+    
+    @staticmethod  
+    def cargar_salidas():
+        salidas = _loader.STAGE.data['salidas']
+        for salida in salidas:
+            sld = Salida(salidas[salida])
+            _loader.STAGE.properties.add(sld,layer=C.CAPA_GROUND_SALIDAS)
+    
 class ChunkMap(DirtySprite):
     #chunkmap: la idea es tener 9 de estos al mismo tiempo.
     def __init__(self,data):
