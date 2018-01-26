ec06c7a4f28d28eafbbb3075a6b56d0a21f562c7
zenieldanaku <danielrossyamitrano@gmail.com>
2018-01-26 12:45:42 -0300

    Nueva propiedad "atributos" en Atribuido + Ahora engloba los atributos que se cargan por data en un dict. + Atribuido ganó un __getatrr__() para buscar en el diccionario los   atributos que se buscan por self.name (como self.velocidad, por   ejemplo)

--- engine/mobs/CompoMob/_atribuido.py
+++ engine/mobs/CompoMob/_atribuido.py
@@ -2,30 +2,17 @@ from engine.base.azoeSprite import AzoeSprite
 
 
 class Atribuido(AzoeSprite):
-    ataque = 0
-    defensa = 0
-    ataque_especial = 0
-    defensa_especial = 0
-    social = 0
-    velocidad = 0  # en pixeles por frame
-    salud = 0  # salud máxima
-    mana = 0  # maná maximo
-    salud_act = 0 # puntos de salud restantes
-    mana_act = 0  # cantidad de magia restante.
+    atributos = None
 
     def __init__(self, *args, **kwargs):
         super().__init__(**kwargs)
-        data = self.data['atributos']
-        self.ataque = data['ataque']
-        self.defensa = data['defensa']
-        self.ataque_especial = data['ataque_especial']
-        self.defensa_especial = data['defensa_especial']
-        self.mana = data['mana']
-        self.social = data['social']
-        self.velocidad = data['velocidad']
-        self.salud = data['salud']
-        self.salud_act = self.salud
-        self.mana_act = self.mana
+        self.atributos = self.data['atributos']
+        self.salud_act = self.atributos['salud']
+        self.mana_act = self.atributos['mana']
+
+    def __getattr__(self, item):
+        if item in self.atributos:
+            return self.atributos[item]
 
     def mover(self, dx, dy):
         dx *= self.velocidad
