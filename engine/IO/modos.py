baf3a36550a15005aa887ed6b10c89d9e3ea7266
zenieldanaku <danielrossyamitrano@gmail.com>
2018-02-03 15:27:50 -0300

    Llegó la eliminación de Modos... - Eliminada la clase Modo. Las tres funciones remanentes (update(),   toggle_mode() y pop_menu()) ahora funcionan a nivel módulo, ya que no   necesitan estar dentro de una clase para nada.
    Lo único que queda de los modos ahora es la variable EngineData.MODO, un
    mero string de lo que supo ser la clase mas larga y compleja del engine.
    
    Yo diría que update() ni se merece estar en un módulo separada. Por lo
    que es, podría estar directamente integrada al mainloop, lo que sería
    algo así como volver a las raices.
    
    Por otra parte, pop_menu() tiene un eval() que deberia desaparecer, y
    desde hace mucho pienso que, siendo una función relativa a los menues,
    debería estar cerca de los menues, no tirada en IO como si fuera algo
    muy diferente.
    
    Y finalmente, me molesta bastante que toggle_mode() responda al evento
    "Key", en lugar de a uno especializado. Quizás eso vuele también en el
    futuro, si encuentro otra forma, más directa, de llamar a los submodos.

--- engine/IO/modos.py
+++ engine/IO/modos.py
@@ -6,60 +6,58 @@ from engine.UI.menues import *
 from engine.UI import QuickCircularMenu
 
 
-class Modo:
-    @staticmethod
-    def update(events):
-        EventDispatcher.process()
-        for event in get_taphold_events(events):
-            EventDispatcher.trigger('Key', 'Modo.'+Ed.MODO, event.__dict__)
+def update(events):
+    EventDispatcher.process()
+    for event in get_taphold_events(events):
+        EventDispatcher.trigger('Key', 'Modo.'+Ed.MODO, event.__dict__)
 
-        return Renderer.update()
-
-    @classmethod
-    def toggle_mode(cls, event):
-        nombre = event.data.get('nom', None)
-        tipo = event.data.get('type', None)
-        origin = event.origin
-
-        if tipo == 'tap' and origin == 'Modo.Aventura':
-            if nombre == 'menu':
-                EventDispatcher.trigger('OpenMenu', origin, {'value': 'Pausa'})
-
-            elif nombre == 'contextual':
-                Ed.HERO.detener_movimiento()
-                QuickCircularMenu(Ed.current_qcm_idx, Md.QMC)
-
-    @classmethod
-    def pop_menu(cls, event):
-        titulo = event.data['value']
-        if titulo == 'Previous':
-            del Ed.acceso_menues[-1]
-            titulo = Ed.acceso_menues[-1]
-        else:
-            Ed.acceso_menues.append(titulo)
-
-        if titulo not in Ed.MENUS:
-            name = 'Menu' + titulo
-            try:
-                if name in Md.custommenus:
-                    menu = Md.custommenus[name]()
-                else:
-                    # from globals()
-                    menu = eval(name + '()')
-
-            except Exception as Description:
-                print('No se pudo abrir el menu porque:', Description)
-                menu = Menu(titulo)
-        else:
-            menu = Ed.MENUS[titulo]
-            menu.reset()
-
-        Ed.MODO = 'Menu'
-        EventDispatcher.trigger('TogglePause', 'Modos', {'value': True})
-        menu.register()
-        Renderer.add_overlay(menu, CAPA_OVERLAYS_MENUS)
-        Renderer.overlays.move_to_front(menu)
+    return Renderer.update()
 
 
-EventDispatcher.register(Modo.pop_menu, 'OpenMenu')
-EventDispatcher.register(Modo.toggle_mode, 'Key')
+def toggle_mode(event):
+    nombre = event.data.get('nom', None)
+    tipo = event.data.get('type', None)
+    origin = event.origin
+
+    if tipo == 'tap' and origin == 'Modo.Aventura':
+        if nombre == 'menu':
+            EventDispatcher.trigger('OpenMenu', origin, {'value': 'Pausa'})
+
+        elif nombre == 'contextual':
+            Ed.HERO.detener_movimiento()
+            QuickCircularMenu(Ed.current_qcm_idx, Md.QMC)
+
+
+def pop_menu(event):
+    titulo = event.data['value']
+    if titulo == 'Previous':
+        del Ed.acceso_menues[-1]
+        titulo = Ed.acceso_menues[-1]
+    else:
+        Ed.acceso_menues.append(titulo)
+
+    if titulo not in Ed.MENUS:
+        name = 'Menu' + titulo
+        try:
+            if name in Md.custommenus:
+                menu = Md.custommenus[name]()
+            else:
+                # from globals()
+                menu = eval(name + '()')
+
+        except Exception as Description:
+            print('No se pudo abrir el menu porque:', Description)
+            menu = Menu(titulo)
+    else:
+        menu = Ed.MENUS[titulo]
+        menu.reset()
+
+    Ed.MODO = 'Menu'
+    EventDispatcher.trigger('TogglePause', 'Modos', {'value': True})
+    menu.register()
+    Renderer.add_overlay(menu, CAPA_OVERLAYS_MENUS)
+    Renderer.overlays.move_to_front(menu)
+
+
+EventDispatcher.register(pop_menu, 'OpenMenu')
+EventDispatcher.register(toggle_mode, 'Key')
