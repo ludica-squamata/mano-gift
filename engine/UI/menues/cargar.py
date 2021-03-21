f2937c99ada63378e92082471a5dc238a50dc66b
danielrossyamitrano <danielrossyamtrano@gmail.com>
2021-03-21 12:51:47 -0300

    Nueva capacidad del menú Cargar + Ahora puede eliminar los archivos no deseados. Cuenta ahora con dos   botones mediante los cuales se puede cargar y eliminar archivos. Se   mantiene la capacidad de cargar archivos directamente mediante Enter.

--- engine/UI/menues/cargar.py
+++ engine/UI/menues/cargar.py
@@ -1,5 +1,5 @@
+from engine.globs import EngineData, ANCHO, ALTO, CANVAS_BG
 from engine.globs.event_dispatcher import EventDispatcher
-from engine.globs import EngineData, ANCHO, ALTO
 from engine.globs.azoe_group import AzoeGroup
 from engine.misc import Config
 from .menu import Menu
@@ -16,30 +16,44 @@ class MenuCargar(Menu):
     def __init__(self):
         super().__init__("Cargar Partida")
         self.functions['tap'].update({
-            'accion': self.cargar,
+            'accion': self.press_button,
             'contextual': self.cancelar,
-            'arriba': lambda: self.elegir_opcion('arriba'),
-            'abajo': lambda: self.elegir_opcion('abajo'),
+            'arriba': lambda: self.direccionar('arriba'),
+            'abajo': lambda: self.direccionar('abajo'),
+            'izquierda': lambda: self.direccionar('izquierda'),
+            'derecha': lambda: self.direccionar('derecha'),
             'menu': self.cargar
         })
         self.functions['hold'].update({
-            'arriba': lambda: self.elegir_opcion('arriba'),
-            'abajo': lambda: self.elegir_opcion('abajo'),
-        })
-        self.functions['release'].update({
-            'accion': self.cargar,
+            'arriba': lambda: self.direccionar('arriba'),
+            'abajo': lambda: self.direccionar('abajo'),
+            'izquierda': lambda: self.direccionar('izquierda'),
+            'derecha': lambda: self.direccionar('derecha'),
         })
 
         self.filas = AzoeGroup('Filas')
-        self.create_draw_space('Elija un archivo', ANCHO - 16, ALTO / 2-6, 11, 65)
+        self.create_draw_space('Elija un archivo', ANCHO - 16, ALTO / 2 - 6, 11, 65)
         self.llenar_espacio_selectivo()
         if len(self.filas):
             self.elegir_opcion(0)
+
+        n, d, c, e = 'nombre', 'direcciones', 'comando', self.draw_space_rect
+        botones = [
+            {n: 'Cargar', d: {'derecha': 'Borrar'}, c: self.cargar, 'pos': [e.centerx - 180, e.bottom + 20]},
+            {n: 'Borrar', d: {'izquierda': 'Cargar'}, c: self.eliminar, 'pos': [e.centerx + 20, e.bottom + 20]}
+        ]
+        self.establecer_botones(botones, 5)
 
     def llenar_espacio_selectivo(self):
         list_dir = os.listdir(Config.savedir)
         self.archivos = [f.split('.')[0] for f in list_dir if f.endswith('.json') and f != 'config.json']
         self.fill_draw_space(self.archivos, self.draw_space_rect.w, 21)
+
+    def direccionar(self, direccion):
+        if direccion in ('arriba', 'abajo'):
+            self.elegir_opcion(direccion)
+        elif direccion in ('derecha', 'izquierda'):
+            self.select_one(direccion)
 
     def elegir_opcion(self, direccion):
         i = 0
@@ -65,6 +79,19 @@ class MenuCargar(Menu):
             self.deregister()
             EventDispatcher.trigger('EndDialog', self, {'layer': self.layer})
 
+    def eliminar(self):
+        current = self.archivos[self.sel] + '.json'
+        ruta = os.path.join(os.getcwd(), 'save', current)
+        os.remove(ruta)
+        spr = self.filas.get_sprite(self.sel)
+        spr.kill()
+        self.opciones -= 1
+        if len(self.filas):
+            self.elegir_opcion(0)
+
     def update(self):
+        self.draw_space.fill(CANVAS_BG)
         self.filas.draw(self.draw_space)
+        self.botones.update()
+        self.botones.draw(self.canvas)
         self.canvas.blit(self.draw_space, self.draw_space_rect)
